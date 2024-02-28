7b580a628b786da5b2c643a2021589a1c454262e
spacewalk01 <selmee006@gmail.com>
2024-02-28 09:46:15 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,66 +13,8 @@
 
 static vector<Scalar> colors;
 
-void format_tracker_input(Mat &frame, vector<Detection> &detections, vector<byte_track::Object> &tracker_objects)
-{
-    const float H = 640;
-    const float W = 640;
-    const float r_h = H / (float)frame.rows;
-    const float r_w = W / (float)frame.cols;
-
-    for (int i = 0; i < detections.size(); i++)
-    {
-        float x = detections[i].bbox.x;
-        float y = detections[i].bbox.y;
-        float width = detections[i].bbox.width;
-        float height = detections[i].bbox.height;
-
-        if (r_h > r_w)
-        {
-            x = x / r_w;
-            y = (y - (H - r_w * frame.rows) / 2) / r_w;
-            width = width / r_w;
-            height = height / r_w;
-        }
-        else
-        {
-            x = (x - (W - r_h * frame.cols) / 2) / r_h;
-            y = y / r_h;
-            width = width / r_h;
-            height = height / r_h;
-        }
-
-        byte_track::Rect<float> rect(x, y, width, height);
-
-        byte_track::Object obj(rect, detections[i].class_id, detections[i].conf);
-
-        tracker_objects.push_back(obj);
-     }
-}
-
-void draw_bboxes(Mat& frame, const vector<byte_track::BYTETracker::STrackPtr> &output)
-{
-    for (int i = 0; i < output.size(); i++)
-    {
-        auto detection = output[i];
-        auto trackId = detection->getTrackId();
-
-        int x = detection->getRect().tlwh[0];
-        int y = detection->getRect().tlwh[1];
-        int width = detection->getRect().tlwh[2];
-        int height = detection->getRect().tlwh[3];
-
-        auto color_id = trackId % colors.size();
-        rectangle(frame, Point(x, y), Point(x + width, y + height), colors[color_id], 3);
-
-        // Detection box text
-        string classString = to_string(trackId);
-        Size textSize = getTextSize(classString, FONT_HERSHEY_DUPLEX, 1, 2, 0);
-        Rect textBox(x, y - 40, textSize.width + 10, textSize.height + 20);
-        rectangle(frame, textBox, colors[color_id], FILLED);
-        putText(frame, classString, Point(x + 5, y - 10), FONT_HERSHEY_DUPLEX, 1, Scalar(0, 0, 0), 2, 0);
-    }
-}
+void draw_bboxes(Mat& frame, const vector<byte_track::BYTETracker::STrackPtr> &output);
+void format_tracker_input(Mat &frame, vector<Detection> &detections, vector<byte_track::Object> &tracker_objects);
 
 int main(int argc, char** argv)
 {
@@ -133,4 +75,66 @@ int main(int argc, char** argv)
     output_video.release();
 
     return 0;
+}
+
+
+void format_tracker_input(Mat &frame, vector<Detection> &detections, vector<byte_track::Object> &tracker_objects)
+{
+    const float H = 640;
+    const float W = 640;
+    const float r_h = H / (float)frame.rows;
+    const float r_w = W / (float)frame.cols;
+
+    for (int i = 0; i < detections.size(); i++)
+    {
+        float x = detections[i].bbox.x;
+        float y = detections[i].bbox.y;
+        float width = detections[i].bbox.width;
+        float height = detections[i].bbox.height;
+
+        if (r_h > r_w)
+        {
+            x = x / r_w;
+            y = (y - (H - r_w * frame.rows) / 2) / r_w;
+            width = width / r_w;
+            height = height / r_w;
+        }
+        else
+        {
+            x = (x - (W - r_h * frame.cols) / 2) / r_h;
+            y = y / r_h;
+            width = width / r_h;
+            height = height / r_h;
+        }
+
+        byte_track::Rect<float> rect(x, y, width, height);
+
+        byte_track::Object obj(rect, detections[i].class_id, detections[i].conf);
+
+        tracker_objects.push_back(obj);
+     }
+}
+
+void draw_bboxes(Mat& frame, const vector<byte_track::BYTETracker::STrackPtr> &output)
+{
+    for (int i = 0; i < output.size(); i++)
+    {
+        auto detection = output[i];
+        auto trackId = detection->getTrackId();
+
+        int x = detection->getRect().tlwh[0];
+        int y = detection->getRect().tlwh[1];
+        int width = detection->getRect().tlwh[2];
+        int height = detection->getRect().tlwh[3];
+
+        auto color_id = trackId % colors.size();
+        rectangle(frame, Point(x, y), Point(x + width, y + height), colors[color_id], 3);
+
+        // Detection box text
+        string classString = to_string(trackId);
+        Size textSize = getTextSize(classString, FONT_HERSHEY_DUPLEX, 1, 2, 0);
+        Rect textBox(x, y - 40, textSize.width + 10, textSize.height + 20);
+        rectangle(frame, textBox, colors[color_id], FILLED);
+        putText(frame, classString, Point(x + 5, y - 10), FONT_HERSHEY_DUPLEX, 1, Scalar(0, 0, 0), 2, 0);
+    }
 }
